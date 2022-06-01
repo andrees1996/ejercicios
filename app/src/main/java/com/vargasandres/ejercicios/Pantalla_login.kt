7102356e32bbe21d52c706d2d451c0b7ed531cb1
andrees1996 <stalin.vargas@epn.edu.ec>
2022-06-01 16:52:12 -0500

    Desarrollo_login

---
+++ app/src/main/java/com/vargasandres/ejercicios/Pantalla_login.kt
@@ -0,0 +1,11 @@
+package com.vargasandres.ejercicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_login)
+    }
+}
